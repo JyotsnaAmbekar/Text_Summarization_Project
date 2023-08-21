4a70a82e216c6ada5b178fe508e70613b26603cd
JyotsnaAmbekar <jyotsnakunte1921@gmail.com>
2023-08-20 19:10:39 -0500

    Transformation, Trainer, Evaluation added

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ sys.path.append('src')
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -23,6 +27,39 @@ try:
     logger.info(">>>> stage {} started <<<<".format(STAGE_NAME))
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(">>>> stage {} completed <<<< \n\n ==================".format(STAGE_NAME))
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(">>>> stage {} started <<<<".format(STAGE_NAME))
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(">>>> stage {} completed <<<< \n\n ==================".format(STAGE_NAME))
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer stage"
+
+try:
+    logger.info(">>>> stage {} started <<<<".format(STAGE_NAME))
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
+    logger.info(">>>> stage {} completed <<<< \n\n ==================".format(STAGE_NAME))
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation stage"
+
+try:
+    logger.info(">>>> stage {} started <<<<".format(STAGE_NAME))
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(">>>> stage {} completed <<<< \n\n ==================".format(STAGE_NAME))
 except Exception as e:
     logger.exception(e)
