1f3bb848d8cad6e76cae14fb8caeb5034adeb303
xyz <xyz@c02gg1twmd6n.attlocal.net>
2023-08-25 22:17:45 -0500

    App details added

--- app.py
+++ app.py
@@ -1,48 +1,50 @@
-#!/usr/bin/env python3
+from fastapi import FastAPI, Form, Request
+import uvicorn
 import sys
 import os
 sys.path.append('src')
-
-from fastapi import FastAPI
-import uvicorn
 from fastapi.templating import Jinja2Templates
-from starlette.responses import RedirectResponse
+from starlette.responses import RedirectResponse, HTMLResponse
 from fastapi.responses import Response
 from textSummarizer.pipeline.prediction import PredictionPipeline
 
-
-text = "What is Text Summarization?"
+text:str = "Text Summarization"
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/", tags=["authentication"])
-def index():
-    return RedirectResponse(url="/docs")
-
-
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "text": "", "summarized_text": ""})
 
 @app.get("/train")
-def training():
+async def training():
     try:
         os.system("python main.py")
         return Response("Training successful !!")
-
     except Exception as e:
-        return Response("Error Occurred! {}".format(e))
-    
+        return Response(f"Error Occurred! {e}")
 
-
-
-@app.post("/predict")
-def predict_route(text):
+@app.post("/predict", response_class=HTMLResponse)
+async def predict_route(request: Request, text: str = Form(...)):
     try:
+        # Debug: Check if the function is being called
+        print("predict_route function called")
 
         obj = PredictionPipeline()
-        text = obj.predict(text)
-        return text
+        summarized_text = obj.predict(text)
+
+        # Debug: Check the summarized_text before rendering the template
+        print("Summarized Text:", summarized_text)
+
+        return templates.TemplateResponse(
+            "index.html",
+            {"request": request, "text": text, "summarized_text": summarized_text}
+        )
     except Exception as e:
         raise e
-    
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
+
